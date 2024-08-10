353219e43a81e7ff9f2ab615d10b44ea3790b57e
rhuallpa <rebeca.huallpa@tecsup.edu.pe>
2024-08-09 19:21:03 -0500

    cambios en navbar

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -53,7 +53,7 @@ export const NavbarRoutes = () => {
         ) : null}
         {isAdminUser && (
           <Button size="sm" variant="ghost" className="bg-[#8e24aa] text-white hover:bg-[#7b1fa2]" onClick={handleAdminClick}>
-            Panel de Administrador
+            Panel de Administración
           </Button>
         )}
         <UserButton afterSignOutUrl="/" />
